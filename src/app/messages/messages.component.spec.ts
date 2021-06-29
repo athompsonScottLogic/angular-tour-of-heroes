2a0b5f94bf1ac448265643a1f0a26b401f0e2f1f
athompsonScottLogic <you@example.com>
2021-06-29 12:39:55 +0100

    added basic component testing to message component

--- src/app/messages/messages.component.spec.ts
+++ src/app/messages/messages.component.spec.ts
@@ -5,6 +5,7 @@ import { MessagesComponent } from './messages.component';
 describe('MessagesComponent', () => {
   let component: MessagesComponent;
   let fixture: ComponentFixture<MessagesComponent>;
+  let h2: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -16,10 +17,22 @@ describe('MessagesComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(MessagesComponent);
     component = fixture.componentInstance;
+    h2 = fixture.nativeElement.querySelector('h2');
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display message title', () => {
+    fixture.detectChanges();
+    expect(h2.textContent).toContain("Messages");
+  });
+
+  it('should display a different message title', () => {
+    h2.textContent = 'Oodles of Noodles';
+    fixture.detectChanges();
+    expect(h2.textContent).toContain('Oodles of Noodles');
   });
 });
