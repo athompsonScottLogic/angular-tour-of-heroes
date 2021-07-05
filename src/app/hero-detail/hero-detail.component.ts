bd547826cb9a3900af6bb547af178c113cbbfbb8
athompsonScottLogic <you@example.com>
2021-07-05 10:18:03 +0100

    HeroDetailComponent retrieves data asynchronously using switchMap

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
+import { switchMap } from 'rxjs/operators';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
@@ -10,24 +12,17 @@ import { HeroService } from '../hero.service';
   templateUrl: './hero-detail.component.html',
   styleUrls: [ './hero-detail.component.scss' ]
 })
-export class HeroDetailComponent implements OnInit {
-  hero: Hero | undefined;
+export class HeroDetailComponent{
+  readonly hero$ = this.route.paramMap
+  .pipe(
+    switchMap(params => this.heroService.getHero(Number(params.get('id'))))
+  );
 
   constructor(
     private route: ActivatedRoute,
     private heroService: HeroService,
     private location: Location
   ) {}
-
-  ngOnInit(): void {
-    this.getHero();
-  }
-
-  getHero(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.heroService.getHero(id)
-      .subscribe(hero => this.hero = hero);
-  }
 
   goBack(): void {
     this.location.back();
