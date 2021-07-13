9034a40e7eaa6efaba8ba36208198f0adf8d87a2
athompsonScottLogic <you@example.com>
2021-07-13 16:11:29 +0100

    updateHero works correctly, removed addHero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -71,9 +71,7 @@ export class HeroService {
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
 
-      // TODO send the error to remote logging infrastructure
       console.error(error);
-
       this.log(`${operation} failed: ${error.message}`);
 
       return of(result as T);
