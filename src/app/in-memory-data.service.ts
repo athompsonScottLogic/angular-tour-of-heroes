246680d382526873f3731ddfe433652e2a54d2fc
athompsonScottLogic <you@example.com>
2021-07-16 16:47:01 +0100

    changed mock heroes back

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -8,16 +8,16 @@ import { Hero } from './hero';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
-      { id: 11, name: 'Dr Nice' },
-      { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      { id: 1, name: '18 Wheel Colin' },
+      { id: 2, name: 'Mr Mango' },
+      { id: 3, name: 'Dam Bogger' },
+      { id: 4, name: 'Yarr van Gogh' },
+      { id: 5, name: 'Dr Hardhead' },
+      { id: 6, name: 'Gilly Guy' },
+      { id: 7, name: 'The Settler' },
+      { id: 8, name: 'Xena: Princess' },
+      { id: 9, name: 'Half Oasis' },
+      { id: 10, name: 'Heartbreaker' }
     ];
     return {heroes};
   }
